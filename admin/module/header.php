2e8e96de505cddc734f290fde3ce2f6b8e274a0c
khangroyals <nguyenvikhang1999@gmail.com>
2021-10-23 19:16:47 +0700

    update

--- admin/module/header.php
+++ admin/module/header.php
@@ -1,38 +1,35 @@
-<div>
-    </form>
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-          <div class="item active">
-            <a href=""></a>
-            <img src="https://www.pnj.com.vn/images/promo/51/Laisuat0-768x450_as3u-48.jpg" alt="Los Angeles" style="width:100%; height: 500px;">
-          </div>
-
-          <div class="item">
-            <img src="https://trangsuc123.com/wordpress/uploads/banner12.jpg" alt="Chicago" style="width:100%; height: 500px;">
-          </div>
-
-          <div class="item">
-            <img src="https://preciousle.com/uploaded/news/6%20banner-trang-suc-1.gif" alt="New york" style="width:100%; height: 500px;">
-          </div>
-        </div>
-
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right"></span>
-          <span class="sr-only">Next</span>
-        </a>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img style="width:100%;height:300px" src="https://images.unsplash.com/photo-1528715471579-d1bcf0ba5e83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1193&q=80" alt="...">
+      <div class="carousel-caption">
+      
       </div>
-
+    </div>
+    <div class="item">
+      <img style="width:100%;height:300px" src="" alt="...">
+      <div class="carousel-caption">
         
+      </div>
+    </div>
+   
+  </div>
 
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
--- chucnang/left/danhmucsp.php
+++ chucnang/left/danhmucsp.php
@@ -67,7 +67,7 @@
             </div>
           </div> -->
 
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3932.0598420457077!2d105.60232731409813!3d9.760998779783517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0f1d1e88956ef%3A0xef7a6de6658fee0c!2zxJDhuqFpIEjhu41jIEPhuqduIFRoxqEgS2h1IEjDsmEgQW4!5e0!3m2!1svi!2s!4v1586271577535!5m2!1svi!2s" width="350" height="500" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3932.0598420457077!2d105.60232731409813!3d9.760998779783517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0f1d1e88956ef%3A0xef7a6de6658fee0c!2zxJDhuqFpIEjhu41jIEPhuqduIFRoxqEgS2h1IEjDsmEgQW4!5e0!3m2!1svi!2s!4v1586271577535!5m2!1svi!2s" width="350" height="500" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> 
 
 
       
